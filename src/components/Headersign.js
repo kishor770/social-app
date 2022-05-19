559b6913c2b250efaa675565b3c5b45ef1ee486d
Kishorechandra Pollai <kishorechandra.p@tigeranalytics.local>
2022-05-19 10:55:05 +0530

    first commit

---
+++ src/components/Headersign.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import './home/Header.css';
+export default function Headersign(){
+    return(
+        <div className="he">
+            <p>Social Connect</p>
+        </div>
+    )
+}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,7 +1,33 @@
-export default function Signup(){
-    return(
-        <div>
-            
-        </div>
-    )
-}
+    import * as React from 'react';
+    import Box from '@mui/material/Box';
+    import TextField from '@mui/material/TextField';
+    import Button from '@mui/material/Button';
+import Headersign from './Headersign';
+import './home/Header.css';
+    
+    export default function Signup() {
+      return (
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <Headersign/>
+            <br/>
+            <div className='si'>
+            <TextField id="outlined-basic" label="Name" variant="outlined" /><br/>
+            <TextField id="outlined-basic" label="Age" variant="outlined" /><br/>
+            <TextField id="outlined-basic" label="Email" variant="outlined" /><br/>
+            <TextField id="outlined-basic" label="Password" variant="outlined" /><br/>
+            <TextField id="outlined-basic" label="Confirm Password" variant="outlined" /><br/>
+            <Button variant="contained">Sign up</Button>
+          </div>
+          </div>
+        </Box>
+      );
+    }
+    
