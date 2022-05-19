3e6e86795d4d11e5a97671b63c44ea84ddc4f8e0
Kishorechandra Pollai <kishorechandra.p@tigeranalytics.local>
2022-05-19 11:22:32 +0530

    first commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,31 @@
-export default function Login(){
-    return(
-        <div>
-            
+
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Headersign from './Headersign';
+import Button from '@mui/material/Button';
+import './home/Header.css';
+
+export default function Login() {
+    return (
+        <Box
+          component="form"
+          sx={{
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <Headersign/>
+            <br/>
+        <div className='si'>
+        <TextField id="outlined-basic" label="Email" variant="outlined" /><br/>
+        <TextField id="outlined-basic" label="Password" variant="outlined" /><br/>
+        <Button variant="contained">Sign up</Button>
         </div>
-    )
+      </div>
+      
+    </Box>
+  );
 }
